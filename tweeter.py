1d09815d4249dcc0db91d9e42897babf4693b9ef
Eli W. Hunter <hunter.eli.w@gmail.com>
2019-02-22 16:12:39 -0500

    Update TweeterBot module docstring

--- tweeter.py
+++ tweeter.py
@@ -3,8 +3,9 @@
 """Send tweets from a twitter account.
 
 This module contains functions enabling easy tweeting from a Twitter account
-using the secrets set up in ''. This also contains a command line interface for
-tweeting.
+using the secrets set up in the twitter_secrets module. This also contains a
+simple command line interface that sends a single tweet for every line in
+stdin or of the files provided as arguments.
 
 Authors:
     Eli W. Hunter
